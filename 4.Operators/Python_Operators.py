81bb6e615e79b389580ff03d74e326eee41d91ea
Devendran D <111166846+d4devendran@users.noreply.github.com>
2022-09-01 17:48:56 +0530

    Add files via upload

---
+++ 4.Operators/Python_Operators.py
@@ -0,0 +1,87 @@
+Operators:
+--> Python Operators in general are used to perform operations on values and variables.
+--> OPERATORS: Are the special symbols. Eg- + , * , /, etc.
+--> OPERAND: It is the value on which the operator is applied.
+
+Types of operators:
+1. Arithmatic operators
+Operator			Name				Example			Result
+															if x=5 , y=10
++					Addition			x+y					10
+-					Subtraction			x-y					-5
+*					Multiplication		x*y					50
+/					Division			x/y					0.5
+%					Modulus				x%y					5
+**					Exponentiation		x**y				9765625
+//					Floor division		x//y				0
+
+2. Assignment operatiors
+Operator			Example				   Same As
+=				    x=5						x=5
++=				    x+=3					x=x+3
+-=				    x-=3					x=x-3
+*=				    x*=3					x=x*3
+/=				    x/=3					x=x/3
+%=				    x%=3					x=x%3
+//=				    x//=3					x=x//3
+**=				    x**=3					x=x**3
+&=				    x&=3					x=x&3
+|=				    x|=3					x=x|3
+^=				    x^=3					x=x^3
+>>==				x>>=3					x=x>>3
+<<==				x<<=3					x=x<<3
+
+3. Comparison operators
+Operator			Name				Example
+==					Equal				x==y
+!=					Not Equal			x!=y
+>					Greater than		x>y
+<					Less than			x<y
+>=					Greater than		x>=y
+					or equal to
+<=					Less than or 		x<=y
+					equal to
+
+4. Logical operators
+Operator			Description					    Example
+
+and					Returns True if both			x<5 and x<10
+					Statements are True
+
+or					Returns True if any				x<5 or x<10
+					of statements is True
+
+not					Reverse the result				not(x<5 and x<10)
+					returns False if the
+					result is true
+
+5. Membership operators
+Operator			Description								   Example
+
+in					Returns True if a sequence					x in y
+					with specified value is present
+					in the object
+
+not in				Returns True if a sequence					x not in y
+					with the specified value is not
+					present in the object
+
+6. Identity operators
+Operator			Name									    Example
+
+is					Returns True if both						x is y
+					variables are same object
+
+is not				Returns True if both						x is not y
+					variables are not the same object
+
+7. Bitwise operators
+Bitwise operators used to compare binary numbers
+
+Operator			Name			Description
+
+&					AND			Sets each bit into 1 if both bits 	are 1
+|					OR			Sets each bit to 1 if one of two bits is 1
+^					XOR			Sets each bit 1 if only one of two bits is 1
+~					NOT			Inverts all the bits
+
---
+++ 4.Operators/Python_Operators_Interview_Questions.py
@@ -0,0 +1,54 @@
+1.KEY WORDS AVAILABLE IN PYTHON
+
+Keyword           Description
+
+and	        --->  A logical operator
+as	        --->  To create an alias
+assert	    --->  For debugging
+break	    --->  To break out of a loop
+class	    --->  To define a class
+continue    ---> To continue to the next iteration of a loop
+def	        ---> To define a function
+del	        ---> To delete an object
+elif	    ---> Used in conditional statements, same as else if
+else	    ---> Used in conditional statements
+except	    ---> Used with exceptions, what to do when an exception occurs
+False	    ---> Boolean value, result of comparison operations
+finally	    ---> Used with exceptions, a block of code that will be executed no matter
+                 if there is an exception or not
+for         --->  To create a for loop
+from        --->  To import specific parts of a module
+global      --->  To declare a global variable
+if	        ---> To make a conditional statement
+import	    ---> To import a module
+in          ---> To check if a value is present in a list, tuple, etc.
+is	        ---> To test if two variables are equal
+lambda	    ---> To create an anonymous function
+None	    ---> Represents a null value
+nonlocal    ---> To declare a non-local variable
+not	        ---> A logical operator
+or	        ---> A logical operator
+pass	    ---> A null statement, a statement that will do nothing
+raise	    ---> To raise an exception
+return	    ---> To exit a function and return a value
+True        ---> Boolean value, result of comparison operations
+try	        ---> To make a try...except statement
+while	    ---> To create a while loop
+with	    ---> Used to simplify exception handling
+yield       ---> To end a function, returns a generator
+
+2.== VS IS OPERATORS
+
+      ==  : is for value equality. It's used to know if two objects have the same value.
+            == is used for value comparison or content comparison or elements comparison.
+      is  : is for reference equality. It's used to know if two references refer (or point) to the same object, i.e if they're identical.
+            is is used for reference comparison.
+
+3. / // % DIFFERENCES
+
+    '/'  Division       --> Divides left hand operand by right hand operand and output in decimals
+    '//' Floor Division	--> The division of operands where the result is the quotient in which the digits
+                            after the decimal point are removed.
+                            But if one of the operands is negative, the result is floored, i.e., rounded away
+                            from zero (towards negative infinity)
+    '%' Modulus         --> It gives remainder value as a result
