bdea5b5f7f47ee4034a47ca5f59a0dc3412d0d3b
Devendran D <111166846+d4devendran@users.noreply.github.com>
2022-09-02 14:33:19 +0530

    Update Python_Loops_Interview_Questions.py

---
+++ 8.Loops/Python_Loops_Interview_Questions.py
@@ -0,0 +1,8 @@
+1. WHILE VS FOR
+
+       While loop – This loop statement checks for a condition at the beginning and till the condition is 
+                    fulfilled, it will execute the body of the loop.
+
+       For loop   – For loops are used to sequentially iterate over a python sequence.
+                    When the sequence has been iterated completely, the for loop ends and thus executes the 
+                    next piece of code.
