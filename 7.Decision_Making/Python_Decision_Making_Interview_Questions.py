ced9d4bd15f752ad39d1766980360fdd5bea5c55
Devendran D <111166846+d4devendran@users.noreply.github.com>
2022-09-02 11:39:08 +0530

    Add files via upload

---
+++ 7.Decision_Making/Python_Decision_Making_Interview_Questions.py
@@ -0,0 +1,11 @@
+1. WHEN TO USE IF ELIF ELSE AND NESTEDIF
+
+ A) if   --> python,if statement is used for decision making operations
+
+    elif --> use the elif condition is used include multipleconditional expressions after the if condition or
+             b/w if and else conditions
+
+    else -->  An if else Python statement evaluates whether an expression is true or false.
+               If a condition is true, the “if” statement executes. Otherwise, the “else” statement executes
+
+   nestedif --> There may be a situation when you want to check for another condition after a condition resolves to true
