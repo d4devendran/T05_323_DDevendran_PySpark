b4c356ebcfc05aa89ac841479384efae38538aa7
Devendran D <111166846+d4devendran@users.noreply.github.com>
2022-09-02 11:49:22 +0530

    Update Python_Decision_Making.py

---
+++ 7.Decision_Making/Python_Decision_Making.py
@@ -0,0 +1,78 @@
+Python Decision making:
+ Decision making is anticipation of conditions occurring while execution of programs and specifying actions were taken according to conditions
+
+	1. Python Program Assumptions
+        Decision Python programming language assumes any
+
+        A.non-zero and not-None values as True, and if it either
+        B.zero or None, then it is assumed as False value
+
+  2. Types of Decision making:
+       A.if
+       B.if else
+       C.if elif else
+       D.Nested if
+        
+        
+      A.if:
+        Syntax:
+
+        if(condition):
+	          <statements>
+
+
+        Ex:
+		    a=19
+	    	if(a==10):
+			      print(“it is a correct value”)
+          
+       B.if else:
+       Syntax:
+
+       if(condition):
+          <statements>
+       else:
+	        <statements>
+       Ex:
+        a=19
+        if(a==10):
+            print(“it is a correct value”)
+	    	else:
+            print(“it is not a correct value”)
+         
+        C.if elif else
+        Syntax:
+        if(condition):
+            <statements>
+        elif(condition):
+            <statements>
+        else:
+		        <statements>
+
+
+        Ex:
+        a=19
+        if(a==10):
+            print(“it is a correct value”)
+	      elif:
+            print(“it is not a correct value”)
+        else:
+            print(“print correct value”)	
+            
+       D.Nested if
+       Nested if is a “if” within a “if” we call it as Nested if
+        Syntax:
+	      if(condition):
+		        <statements>
+	      else(condition):
+		        <statements>
+		      if(condition):
+			        <statements>
+		      else(condition):
+			        <statements>
+
+
+
+	
+
+  
