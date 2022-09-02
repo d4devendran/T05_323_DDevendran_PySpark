d2f61fdcfa92eebf5a9b8ced03453f58df1b4347
Devendran D <111166846+d4devendran@users.noreply.github.com>
2022-09-02 07:16:06 +0530

    Add files via upload

---
+++ 3.IDE/Python_IDE.py
@@ -0,0 +1,41 @@
+Text Editing:
+Key                     Description
+Tab                     Indent or Block Indent
+Shift + Tab             Block un-Indent
+Control + C             Copy
+Control + V             Paste
+Control + A             Select all
+Control + Z             Undo
+Control + X             Cut
+Control + /             Comment
+Control + F             Find
+Control + K             Find Next
+Shift + Control + K     Find Previous
+Control + Backspace     Remove Word Left
+Control + =             Larger Font
+Shift + Control + =     Smaller Font
+
+Selection:
+Keys                    Description
+Shift + Down Arrow      Highlight down
+Shift + Up Arrow        Highlight up
+Shift + Left Arrow      Select Left
+Shift + Right Arrow     Select Right
+
+Terminal:
+Keys                    Description
+Alt + S                 Switch to Terminal or File
+Control + C             Abort Command or Program
+
+Navigation:
+Keys                    Description
+Control + W             Close Pane
+Shift + Control         Previous Pane
+Control + ]             Goto Right Tab
+Control + [             Goto Left Tab
+
+File:
+Keys                    Description
+Control + S             Save
+Control + N             New File
+Shift + Control + S     Save As
